d912726059ae0e89dfe0fc21611dbbee048119e6
CsarChvz <cesarchavezrodriguez9@gmail.com>
2022-06-21 13:50:18 -0500

    Migracion puests

--- store/models.py
+++ store/models.py
@@ -46,7 +46,7 @@ class Customer(models.Model):
         (MEMBERSHIP_SILVER, 'Silver'),
         (MEMBERSHIP_GOLD, 'Gold'),
     ]
-    first_name = models.CharField(max_length=255)
+    given_name = models.CharField(max_length=255)
     
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
